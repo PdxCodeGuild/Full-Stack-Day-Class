4d8501cd964c904b94f2c576f55af8fe56dbd903
David Selassie <selassid@gmail.com>
2016-05-16 15:46:06 -0700

    Maybe working remarkrc.

---
+++ .remarkrc.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  "plugins": {
+    "remark-lint": {
+      "final-newline": false,
+      "maximum-line-length": false,
+      "ordered-list-marker-value": "single",
+      "strong-marker": "*",
+      "unordered-list-marker-style": "*"
+    },
+    "remark-validate-links": {}
+  }
+};
