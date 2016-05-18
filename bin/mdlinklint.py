9345018d3c033d1c0aa9d429941199e99d379692
David Selassie <selassid@gmail.com>
2016-05-18 11:22:05 -0700

    Tests Markdown link linter.

--- bin/mdlinklint.py
+++ bin/mdlinklint.py
@@ -35,7 +35,7 @@ def get_git_repo_root(dir_path):
         response_bytes = subprocess.check_output(
             'git rev-parse --show-toplevel',
             shell=True)
-    return response_bytes.decode('utf-8')
+    return response_bytes.decode('utf-8').strip()
 
 
 def get_parent_dir(child_path):
@@ -48,15 +48,17 @@ def get_parent_dir(child_path):
     return parent_dir_path
 
 
-def follow_path(path_rel_to_start_dir, start_dir):
+def follow_path(path_rel_to_root_dir, root_dir):
     """For a path relative to a second path, return the resulting path.
 
     >>> follow_path('../otherchild', 'parent/child')
     'parent/otherchild'
-    >>> follow_path('/parent', '/grandparent')
-    '/grandparent/parent'
+    >>> follow_path('/parent', 'grandparent')
+    'grandparent/parent'
     """
-    return path.normpath(path.join(start_dir, path_rel_to_start_dir))
+    if path.isabs(path_rel_to_root_dir):
+        path_rel_to_root_dir = path_rel_to_root_dir[1:]
+    return path.normpath(path.join(root_dir, path_rel_to_root_dir))
 
 
 def parse_dest(dest):
@@ -82,47 +84,67 @@ def parse_dest(dest):
         return ('url', dest)
 
 
-def print_line(file_path, line_num, char_num, text):
-    print('{}:{}:{} {}'.format(file_path, line_num, char_num, text))
+def print_link(link):
+    """Prints out the location of a link from a link tuple.
+
+    >>> print_link(('file.md', 1, 8, 'location', '[a link](location)'))
+    file.md:1:8 [a link](location)
+    """
+    file_path, line_num, char_num, dest, match_text = link
+    print('{}:{}:{} {}'.format(file_path, line_num, char_num, match_text))
 
 
-def find_all_links(lines):
+def find_all_links(lines, file_path):
     """Go through the lines of a Markdown file and return info about all of the
-    links found.
+    links found as a link tuple.
 
-    >>> find_all_links(['This is [a link](location).'])
-    (1, 8, 'location', '[a link](location)')
+    >>> list(find_all_links(['This is [a link](location).'], 'file.md'))
+    [('file.md', 1, 8, 'location', '[a link](location)')]
     """
     for line_num, line in enumerate(lines):
         for match in MD_LINK_RE.finditer(line):
             match_text = match.group()
             char_num = match.start()
             dest = match.group(2)
-            yield (line_num + 1, char_num, dest, match_text)
+            yield (file_path, line_num + 1, char_num, dest, match_text)
+
+
+def is_valid_link(link):
+    """Returns if a link tuple represents a valid link.
+    Looks to the file system for the destination path.
+    """
+    file_path, line_num, char_num, dest, match_text = link
+    file_dir_path = get_parent_dir(file_path)
+    link_type, dest_path = parse_dest(dest)
+    if link_type == 'rel':
+        check_path = follow_path(dest_path, file_dir_path)
+    elif link_type == 'abs':
+        repo_dir_path = get_git_repo_root(file_dir_path)
+        check_path = follow_path(dest_path, repo_dir_path)
+    else:
+        return True
+    return path.exists(check_path)
+
+
+def find_broken_links(file_path):
+    """Open a Markdown file and return a list of all of the broken link tuples.
+    """
+    with open(file_path) as file_lines:
+        return [
+            link
+            for link
+            in find_all_links(file_lines, file_path)
+            if not is_valid_link(link)
+        ]
 
 
 def main():
-    errors_exist = False
+    any_broken = False
     for file_path in sys.argv[1:]:
-        file_dir_path = get_parent_dir(file_path)
-        with open(file_path) as fp:
-            for link in find_all_links(fp):
-                line_num, char_num, dest, match_text = link
-                link_type, dest_path = parse_dest(dest)
-                if link_type == 'rel':
-                    check_path = follow_path(dest_path, file_dir_path)
-                    broken_link = not path.exists(check_path)
-                elif link_type == 'abs':
-                    repo_dir_path = get_git_repo_root(file_dir_path)
-                    check_path = follow_path(dest_path, repo_dir_path)
-                    broken_link = not path.exists(check_path)
-                else:
-                    broken_link = False
-
-                if broken_link:
-                    print_line(file_path, line_num, char_num, match_text)
-                    errors_exist = True
-    return 1 if errors_exist else 0
+        for broken_link in find_broken_links(file_path):
+            print_link(broken_link)
+            any_broken = True
+    return 1 if any_broken else 0
 
 
 if __name__ == '__main__':
