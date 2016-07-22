51166641db633ad3c09694ef803a697dbe044852
David Selassie <selassid@gmail.com>
2016-07-22 16:42:06 -0700

    Refactors roster demo to use modules.

--- demos/rosters/main.py
+++ demos/rosters/main.py
@@ -2,45 +2,7 @@
 
 from student import Student
 from roster import Roster
-
-
-def collect_emails(students):
-    """Collects student emails from a list of students.
-
-    >>> collect_emails([
-    ...     Student('David', 'david@david.com'),
-    ...     Student('Helen', 'helen@helen.com'),
-    ... ])
-    ['david@david.com', 'helen@helen.com']
-    """
-    return [student.email for student in students]
-
-
-def write_intro_email(roster):
-    # Remember, if a doctest has backslashes in it, raw-escape the whole docstring with `r`.
-    r"""Given a class roster, return the intro email.
-
-    The intro email
-
-    >>> print(write_intro_email(Roster(
-    ...     [Student('David', 'david@david.com'), Student('Helen', 'helen@helen.com')],
-    ...     'Meg',
-    ... )))  # Call print() so that the output doesn't need quotes. Doctest just compares output.
-    ... # Use `<BLANKLINE>` to represent a blank line in the output.
-    To: david@david.com,helen@helen.com
-    <BLANKLINE>
-    Meg will be teaching the class.
-    There are 2 students in it.
-    """
-    return (
-        'To: {}\n\n' +
-        '{} will be teaching the class.\n' +
-        'There are {} students in it.'
-    ).format(
-        ','.join(collect_emails(roster.students)),
-        roster.instructor_name,
-        len(roster.students),
-    )
+from roster import write_intro_email
 
 
 def main():
--- demos/rosters/roster.py
+++ demos/rosters/roster.py
@@ -42,3 +42,42 @@ class Roster:
             self.students == other.students and
             self.instructor_name == other.instructor_name
         )
+
+
+def collect_emails(students):
+    """Collects student emails from a list of students.
+
+    >>> collect_emails([
+    ...     Student('David', 'david@david.com'),
+    ...     Student('Helen', 'helen@helen.com'),
+    ... ])
+    ['david@david.com', 'helen@helen.com']
+    """
+    return [student.email for student in students]
+
+
+def write_intro_email(roster):
+    # Remember, if a doctest has backslashes in it, raw-escape the whole docstring with `r`.
+    r"""Given a class roster, return the intro email.
+
+    The intro email
+
+    >>> print(write_intro_email(Roster(
+    ...     [Student('David', 'david@david.com'), Student('Helen', 'helen@helen.com')],
+    ...     'Meg',
+    ... )))  # Call print() so that the output doesn't need quotes. Doctest just compares output.
+    ... # Use `<BLANKLINE>` to represent a blank line in the output.
+    To: david@david.com,helen@helen.com
+    <BLANKLINE>
+    Meg will be teaching the class.
+    There are 2 students in it.
+    """
+    return (
+        'To: {}\n\n' +
+        '{} will be teaching the class.\n' +
+        'There are {} students in it.'
+    ).format(
+        ','.join(collect_emails(roster.students)),
+        roster.instructor_name,
+        len(roster.students),
+    )
